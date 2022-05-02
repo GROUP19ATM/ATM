68190952247df4817d7c88be54696a75bd78f390
GROUP19ATM <104689759+GROUP19ATM@users.noreply.github.com>
2022-05-02 16:53:07 +0530

    GUI

--- atm.py
+++ atm.py
@@ -8,8 +8,45 @@ import insurancepremium
 import updateaccount
 import changepin
 import loanapp
+import os;
+import time;
+from termcolor import colored;
+from pyfiglet import Figlet;
 #import authenticator 
+def screen_clear():
+   # for mac and linux(here, os.name is 'posix')
+   if os.name == 'posix':
+      _ = os.system('clear')
+   else:
+      _ = os.system('cls')
+def loading():
+    bar = [
+    " [=                 ]",
+    " [ =                ]",
+    " [  =               ]",
+    " [   =              ]",
+    " [    =             ]",
+    " [     =            ]",
+    " [      =           ]",
+    " [       =          ]",
+    " [        =         ]",
+    " [         =        ]",
+    " [          =       ]",
+    " [           =      ]",
+    " [            =     ]",
+    " [             =    ]",
+    " [              =   ]",
+    " [               =  ]",
+    " [                = ]",
 
+    ]   
+    
+    x=0
+    while True:
+        print(bar[x % len(bar)], end="\r")
+        time.sleep(.2)
+        x += 1
+        if(x==20):break
 class ATM:
     atmId="abc"
     branchCode="bbb"
@@ -17,12 +54,28 @@ class ATM:
     def __init__(self,cash):
         self.cash=cash
 
+    def welcome(self):
+        f=Figlet(font='banner3-D')
+        
+                                                                                    
+        print(colored(f.renderText('Welcome'),'red'))
+        
     def getId(self):
         return ATM.atmId
      
     def insertCard(self):
-        print("Please insert card in the ATM")
-        return True
+        print("       #########################################")
+        print("       #                                       #");
+        print("       #      Enter Card Number:",end="")
+        c_no=input()
+        #print("#")
+        print("       #                                       #");
+        print("       #      Enter PIN:",end="")
+        pin=input()
+        print("       #                                       #");
+        print("       #########################################")
+        #print("#")
+        return c_no
     
     def readCard(self,accno,pin):
         return True
@@ -38,81 +91,119 @@ class ATM:
             
     def displayCash(self):
         print("The Cash present in ATM is : "+self.cash)
-
+    def getACno(card_no):
+        acno=60
+        return acno
     def readPin(self,pin):
         self.pin=pin
 
     def getAccountNumber(self,acc_no):
         self.acc_no=acc_no
+    def ejectCart():
+        print()
+        print("               Please Remove Your Card        ");
+        print()
+        input()
 
 
     
 obj=ATM(10000)
+obj.welcome();
+while(true):
+    c_no=obj.insertCard()
+    acc_no=obj.getAccountNumber(c_no)
+    pin=60
+    print("\nAuthenticating ")
+    loading()
 
-if obj.insertCard():
-    acc_no=input("Enter Account Number : ")
-    pin=int(input("Enter the pin : "))
-    
     if obj.readCard(acc_no,pin):
         while 1:
-            print("Choose from the given services : ")
-            print("1.Withdraw")
-            print("2.Deposit")
-            print("3.Check Balance")
-            print("4.Cheque Deposit")
-            print("5.Fund Transfer")
-            print("6.Income Tax Payment")
-            print("7.Insurance Premium Payment")
-            print("8.Update Account Details")
-            print("9.Change PIN")
-            print("10.Loan Application Initiation")
-            print("11.Log Out")
+            screen_clear()
+            print("###########################################")
+            print("#   ",end="")
+            print(colored("Choose from the given services :",'red'),end="")
+            print("      #")
+            print("#-----------------------------------------#")
+            print("#   1.Withdraw                            #")
+            print("#   2.Deposit                             #")
+            print("#   3.Check Balance                       #")
+            print("#   4.Cheque Deposit                      #")
+            print("#   5.Fund Transfer                       #")
+            print("#   6.Income Tax Payment                  #")
+            print("#   7.Insurance Premium Payment           #")
+            print("#   8.Update Account Details              #")
+            print("#   9.Change PIN                          #")
+            print("#   10.Loan Application Initiation        #")
+            print("#   11.Log Out                            #")
+            print("###########################################")
         
-            choice=int(input("Enter your choice : (enter the respective number of the services list above)"))
+            print(colored("\n\nEnter your choice (NUMBER):)",'green'))
+            choice=int(input())
             if choice==1:
+                screen_clear()
+                print("WITHDRAW")
                 amt=int(input("Enter the amount to withdraw : "))
                 o1=withdraw.WITHDRAW(acc_no,amt)
                 o1.withdraw()
+                obj.ejectCart();
             
             elif choice==2:
+                screen_clear()
                 amt=int(input("Enter the amount to deposit : "))
                 o2=deposit.DEPOSIT(acc_no,amt)
                 o2.deposit()
+                obj.ejectCart();
             
             elif choice==3:
+                screen_clear()
                 o3=checkbalance.CHECKBALANCE(acc_no)
                 o3.checkBalance()
+                obj.ejectCart();
 
             elif choice==4:
+                screen_clear()
                 cheque_no=int(input("Enter the Cheque Number : "))
                 o4=chequedeposit.CHEQUEDEPOSIT(acc_no,cheque_no)
                 o4.chequeDeposit()
+                obj.ejectCart();
 
             elif choice==5:
+                screen_clear()
                 o5=fundtransfer.FUNDTRANSFER()
                 o5.fundTransfer()
+                obj.ejectCart();
 
             elif choice==6:
+                screen_clear()
                 inc_id=input("Enter the Income TAx ID : ")
                 o6=incometax.INCOMETAXPAYMENT(inc_id)
                 o6.incomeTaxPayment()
+                obj.ejectCart();
 
             elif choice==7:
+                screen_clear()
                 ins_id=input("Enter Insurance Premium ID : ")
                 o7=insurancepremium.INSURANCEPREMIUM(ins_id)
                 o7.insurancePremiumPaytemt()
+                obj.ejectCart();
 
             elif choice==8:
+                screen_clear()
                 o8=updateaccount.UPDATEACCDETAILS()
                 o8.updateAccountDetails()
+                obj.ejectCart();
             elif choice==9:
+                screen_clear()
                 o_pin=int(input("Enter old pin : "))
                 n_pin=int(input("Enter new pin: "))
                 o9=changepin.CHANGEPIN(acc_no,o_pin,n_pin)
                 o9.changePin()
+                obj.ejectCart();
             elif choice==10:
+                screen_clear()
                 o10=loanapp.LOANAPLICATION()
                 o10.loanApplictionInitiation()
+                obj.ejectCart();
 
             elif choice==11:
                 print("LOGGED OUT! ")
--- incometax.py
+++ incometax.py
@@ -1,49 +1,8 @@
-import mysql.connector
-import numpy as np
+class INCOMETAXPAYMENT:
+    def __init__(self,inc_id):
+        self.inc_id=inc_id
 
-class IncomeTax:
-
-    def __init__(self, account_number):
-        self.account_number = account_number
-        self.incomeTax = self.fetchIncomeTaxID()
-
-    def fetchIncomeTaxID(self):
-        try:
-            db = mysql.connector.connect(
-                host='byhmrkozld7aa4b3kupq-mysql.services.clever-cloud.com',
-                user='uhhcevmfsdoh3b14',
-                password='iI4zqX9mPaJHPKmFyjhp',
-                database='byhmrkozld7aa4b3kupq'
-            )
-            cursor = db.cursor()
-            cursor.execute(f"SELECT incomeTaxID FROM INCOME_TAX WHERE accountNumber = '{self.account_number}'")
-            records = cursor.fetchall()
-            return records[0][0]
-
-        except mysql.connector.Error as error:
-            print(f"Unable to fetch Income Tax ID")
-
-    def fetchBalance(self, accountNumber):
-        try:
-            db = mysql.connector.connect(
-                host='byhmrkozld7aa4b3kupq-mysql.services.clever-cloud.com',
-                user='uhhcevmfsdoh3b14',
-                password='iI4zqX9mPaJHPKmFyjhp',
-                database='byhmrkozld7aa4b3kupq'
-            )
-            cursor = db.cursor()
-            cursor.execute(f"SELECT balance FROM ACCOUNT WHERE accountNumber = '{accountNumber}'")
-            records = cursor.fetchall()
-
-            return records[0][0]
-
-        except mysql.connector.Error as error:
-            print("Failed to fetch Balance : {}".format(error))
-
-    def fetchIncomeTaxReturns(self):
-        balance = self.fetchBalance(self.account_number)
-        income_tax_returns = np.random.randint(low=10000, high=int(balance * 3 / 10))
-        print(income_tax_returns)
-
-i = IncomeTax('PDFJB00000000001')
-i.fetchIncomeTaxReturns()
+    def incomeTaxPayment(self):
+        print("income tax id is "+self.inc_id)
+    
+    
