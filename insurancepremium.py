de1f1ba04abf9f8c71a922caf1964ee6d79456a0
GROUP19ATM <group19.atm@gmail.com>
2022-05-02 15:45:04 +0530

    Added Insurance and IncomeTax functionalities

--- insurancepremium.py
+++ insurancepremium.py
@@ -1,8 +1,52 @@
-class INSURANCEPREMIUM:
-    def __init__(self,ins_id):
-        self.ins_id=ins_id
+import mysql.connector
+import numpy as np
 
-    def insurancePremiumPaytemt(self):
-        print("INSURANCE ID is "+self.ins_id)
+class Insurance:
+    def __init__(self, account_number):
+        self.account_number = account_number
+        self.insuranceID = self.findInsuranceID()
+
+    def findInsuranceID(self):
+        try:
+            db = mysql.connector.connect(
+                host='byhmrkozld7aa4b3kupq-mysql.services.clever-cloud.com',
+                user='uhhcevmfsdoh3b14',
+                password='iI4zqX9mPaJHPKmFyjhp',
+                database='byhmrkozld7aa4b3kupq'
+            )
+            cursor = db.cursor()
+            cursor.execute(f"SELECT insuranceID from INSURANCE WHERE accountNumber='{self.account_number}'")
+            records = cursor.fetchall()
+            return records[0][0]
+        except mysql.connector.Error as error:
+            print(f"Failed to fetch insurance details: {error}")
+
+    def fetchBalance(self, accountNumber):
+        try:
+            db = mysql.connector.connect(
+                host='byhmrkozld7aa4b3kupq-mysql.services.clever-cloud.com',
+                user='uhhcevmfsdoh3b14',
+                password='iI4zqX9mPaJHPKmFyjhp',
+                database='byhmrkozld7aa4b3kupq'
+            )
+            cursor = db.cursor()
+            cursor.execute(f"SELECT balance FROM ACCOUNT WHERE accountNumber = '{accountNumber}'")
+            records = cursor.fetchall()
+
+            return records[0][0]
+
+        except mysql.connector.Error as error:
+            print("Failed to fetch Balance : {}".format(error))
+
+    def fetchInsurancePremium(self):
+        balance = self.fetchBalance(self.account_number)
+
+        insurance_premium = np.random.randint(low=10000, high=int(balance * 3 / 10))
+        print(insurance_premium)
+
+
+i = Insurance('PDFJB00000000001')
+i.fetchInsurancePremium()
+
     
     
